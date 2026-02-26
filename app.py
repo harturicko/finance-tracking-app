801e6bf667de706e3d7c2ac8cc48d0b724956d8f
Artur Omelian <harturicko@gmail.com>
2026-02-26 15:55:01 +0100

    commit

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:1234@localhost:5433/mydb'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir,'instance/app.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir,'instance', 'db.sqlite')
 db.init_app(app)
 
 @app.route("/")
